15278d835422412d6f8c0ea8b66768455957321f
danellotpz <denvasik05@gmail.com>
2025-06-23 18:40:06 +0300

    тестовое pwa приложение с иконками

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  pwa: {
+    name: 'Vue PWA App',
+    themeColor: '#42b983',
+    backgroundColor: '#ffffff',
+    display: 'standalone',
+    start_url: '.',
+    manifestOptions: {
+      short_name: 'VuePWA',
+      icons: [
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: './img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+    workboxPluginMode: 'GenerateSW',  // автоматическая генерация service worker
+    workboxOptions: {
+      skipWaiting: true,
+      clientsClaim: true,
+    },
+  },
 });
